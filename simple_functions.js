c233d1418f1540cd53f186558e6ef36513b8c7ba
Toria Sellers <victoria.12292001@gmail.com>
2023-10-23 11:42:11 -0400

    js files

---
+++ simple_functions.js
@@ -0,0 +1,27 @@
+
+function favoriteThings(){
+    console.log("My name is Victoria Sellers.\nMy favorite movie is La La Land with Ryan Gosling and Emma Stone.\nMy favorite band is the Arctic Monkeys");
+    
+}
+
+favoriteThings();
+
+function whyImHere(){
+    let phrase = "I joined the learn to code because I want to be an empowered career woman. I also would like to have more than 10 dollars in my bank account."
+
+    console.log(phrase);
+}
+
+whyImHere();
+
+function favoritePlaces(){
+    let placeA = "The Weeki Wachee Preserves";
+    let placeB = "Homosassa Springs State Park";
+    let placeC = "My hometown, Miramar Florida"
+
+    let phrase = `My favorite places are ${placeA}, ${placeB}, and ${placeC} I love being by water and the ocean.\nI have a deep appreciation for marine life.`
+
+    console.log(phrase)
+}
+
+favoritePlaces();
